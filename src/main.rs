aa1f3450e26f9557bf5115dcccb5e54a804dceba
7wf <7ywolf@gmail.com>
2020-05-15 19:20:49 -0300

    && operator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 #[macro_use]
 extern crate colour;
 
+use std::process;
+
 mod commander;
 mod commands;
 mod ui;
+
+/**
+ * Runs a command.
+ */
+fn run_command(command_name: &str, command_arguments: Vec<&str>) -> Result<bool, String> {
+    match command_name {
+        "exit" => process::exit(0),
+        "cd" => match commands::change_directory::execute(command_arguments) {
+            Ok(_) => Ok(true),
+            Err(error) => Err(error),
+        },
+        command_name => {
+            commander::run_command(command_name, command_arguments);
+            Ok(true)
+        },
+    }
+}
 
 fn main() {
     loop {
@@ -12,16 +31,15 @@ fn main() {
             continue;
         }
 
-        let (command_name, command_arguments) = commander::parse_command_request(&input);
-        let command_arguments: Vec<&str> = command_arguments.collect();
+        let input_commands = input.split("&&");
+        for input_command in input_commands {
+            let (command_name, command_arguments) = commander::parse_command_request(&input_command);
+            let command_arguments: Vec<&str> = command_arguments.collect();
 
-        match command_name {
-            "exit" => return,
-            "cd" => match commands::change_directory::execute(command_arguments) {
+            match run_command(command_name, command_arguments) {
                 Ok(_) => continue,
-                Err(error) => eprintln!("{}", error),
-            },
-            command_name => commander::run_command(command_name, command_arguments),
+                Err(error) => eprintln!("{}", error)
+            }            
         }
     }
 }
